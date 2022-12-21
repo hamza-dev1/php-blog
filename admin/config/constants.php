592737e5e217984367d78b8ae8f67effe08225a7
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-12-21 21:00:03 +0100

    Define database configuration

---
+++ admin/config/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+define('ROOT_URL', 'http://localhost/blog/');
+
+// db config
+define('DB_HOST', 'localhost');
+define('DB_USER', 'hamza');
+define('DB_PASS', 'hamza');
+define('DB_NAME', 'php-blog');
